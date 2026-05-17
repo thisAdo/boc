ead6da35c707f91d6505a7aff467fdb0679fc3a7
Ado <meadonix@gmail.com>
2026-05-17 10:53:13 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   prefix: '.',
   sessionDir: './sesiones',
-  databasePath: './lib/xdroid.json',
+  databaseUrl: 'postgresql://neondb_owner:npg_ASO7lYNCn6BM@ep-bitter-bonus-aq0sg4hk-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   legacyGroupEventsPath: './lib/group-events.json',
   browser: ['Ubuntu', 'Chrome', '120.0.0.0'],
   pairing: {
